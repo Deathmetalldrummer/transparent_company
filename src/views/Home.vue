b8c935e84ebdce6826995ad236d28c429820d95a
Ragnarok <DeathMetallDrummer@gmail.com>
2020-01-11 22:42:15 +0600

    Dashboard

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,28 @@
 <template lang="pug">
-    h1 Home
+    v-app
+        v-app-bar(clipped-left app)
+            v-toolbar-title Transparent
+        v-navigation-drawer(clipped mini-variant permanent app overflow)
+            template(v-slot:append)
+                v-list
+                    v-list-item(link @click='exitToApp')
+                        v-list-item-icon
+                            v-icon exit_to_app
+                        v-list-item-content
+                            v-list-item-title exit
+        v-footer(inset app)
+            span.px-4 &copy; {{ new Date().getFullYear() }}
+
 </template>
 
 <script>
-
-export default {
-  name: 'home',
-  components: {
-  }
-}
+    export default {
+        data: () => ({
+        }),
+        methods: {
+            exitToApp(){
+                console.log(true)
+            }
+        }
+    }
 </script>
